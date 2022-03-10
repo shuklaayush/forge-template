986ac1524864934a1569e4bb08761104cd9af63d
Ayush Shukla <shuklaayush247@gmail.com>
2022-03-10 18:31:06 +0530

    chore: foundry init

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
-import "ds-test/test.sol";
+import {DSTest} from "ds-test/test.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 contract ContractTest is DSTest {
+    Vm internal constant vm = Vm(HEVM_ADDRESS);
+
     function setUp() public {}
 
     function testExample() public {
